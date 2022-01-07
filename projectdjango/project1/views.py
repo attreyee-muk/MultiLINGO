a0a1a0697c4eeaf77cd7730527c3f25ad34a1480
LazerRazor <gaurishbaliga@gmail.com>
2022-01-07 23:11:08 +0530

    Co-authored-by: Attreyee Mukherjee <a-muk@users.noreply.github.com>

--- projectdjango/project1/views.py
+++ projectdjango/project1/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from googletrans import Translator
+
+
 def home(request):
     return render (request, 'home.html')
 # Create your views here.
 
+def translate(request):
+    return render(request,'translate.html')
+
+
+def trans(request):
+    ti = request.GET["text"]
+    translater = Translator()
+    out=translater.translate(ti,dest="en")
+    return render(request,'answer.html',{'texty': out.text })
